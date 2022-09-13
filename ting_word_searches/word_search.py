234f176a34fdffd86ccbd52f0e1fb8b592f2b752
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-09-13 20:04:18 -0300

    feat: inicia implementação da função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    lines_numbers = []
+    files_list = []
+    for file in instance.data:
+        for i, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                lines_numbers.append({"linha": i + 1})
+        files_list.append({
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": lines_numbers,
+            "palavra": word,
+        })
+    if len(lines_numbers) == 0:
+        return []
+    else:
+        return files_list
 
 
 def search_by_word(word, instance):
